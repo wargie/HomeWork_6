39e7ede3bf693d5caffc3aeaed3a42097132449b
wargie <wargie@bk.ru>
2022-05-19 07:24:45 +0200

    дополнен код к задаче №011

--- Task_011/Task_011.py
+++ Task_011/Task_011.py
@@ -1,7 +1,8 @@
-import random
-n = int(input('Введите число: '))
+from random import randint
 
 def get_degree(n):
     return [((-3)**i) for i in range(n)]
+
+n = randint(1, 20)
 
 print (get_degree(n))
